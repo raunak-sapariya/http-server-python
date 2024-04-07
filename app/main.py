1b8fab0f5a795c54959546800f50c9107f443446
raunak-sapariya <raunaksapariya@gmail.com>
2024-04-07 08:42:00 +0530

    finall

--- app/main.py
+++ app/main.py
@@ -1,40 +1,25 @@
 import socket
 
-
 def Request(data):
-    data_str = data.decode()
-    split_lines = data_str.split("\r\n")
-    method, path, version = split_lines[0].split()
-    return method, path, version
-
+        data_str = data.decode()
+        lines = data_str.split("\r\n")
+        method, path, version = lines[0].split()
+        return data_str,lines,method
 
 def main():
     server_socket = socket.create_server(("0.0.0.0", 4221))
     while True:
-        client_con, add = server_socket.accept()
-
-        with client_con:
-            print("Client_Connection_Detail  -->", client_con)
-            print("Client_Address_Detail -->", add)
-            print(f'Connected by {add}')
-
-            data = client_con.recv(1024)
-            print("Data -->", data)
-
-
-           
-
-            method, path, version = Request(data)
-            if path == "/":
-                response1 = b"HTTP/1.1 200 OK\r\n\r\n"
-                
+        client_conn, addr = server_socket.accept()
+        with client_conn:
+            print("Connected by", addr)
+            data = client_conn.recv(1024)
+            print(data)
+            req = Request(data)
+            if req[1] == "/":
+                response = "HTTP/1.1 200 OK\r\n\r\nHello, World!"
             else:
-                response1 = b"HTTP/1.1 404 Not Found\r\n\r\n"
-            print(response1)    
-            client_con.sendall(response1)
-
-    server_socket.close()
-
+                response = "HTTP/1.1 404 Not Found\r\n\r\nPage Not Found"
+            client_conn.send(response.encode())
 
 if __name__ == "__main__":
-   main()
+    main()
