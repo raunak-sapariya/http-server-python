b51b3ba3f5cf315509a535322f18a8b20e6443ca
raunak-sapariya <raunaksapariya@gmail.com>
2024-04-07 09:21:43 +0530

    finalfinal

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 import socket
 
 def Request(data):
-        data_str = data.decode()
-        lines = data_str.split("\r\n")
-        method, path, version = lines[0].split()
-        return method,path,version
+    data_str = data.decode()
+    lines = data_str.split("\r\n")
+    method, path, version = lines[0].split()
+    return method, path, version
 
 def main():
     server_socket = socket.create_server(("0.0.0.0", 4221))
@@ -12,20 +12,17 @@ def main():
         client_conn, addr = server_socket.accept()
         with client_conn:
             print("Connected by", addr)
-
             data = client_conn.recv(1024)
             print(data)
-
-
             req = Request(data)
             print(req)
-            if req[1] == "/":
+            if req[1].startswith("/echo/"):
+                response = "HTTP/1.1 200 OK\r\n\r\n" + req[1][6:]
+                client_conn.send(response.encode())
+            elif req[1] == "/":
                 client_conn.send(b"HTTP/1.1 200 OK\r\n\r\nHello, World!")
-            elif req[1].startwith("/echo/"):
-                client_conn.send(b"HTTP/1.1 200 OK\r\n\r\n" + req[1][6:])
             else:
                 client_conn.send(b"HTTP/1.1 404 Not Found\r\n\r\nPage Not Found")
-            
 
 if __name__ == "__main__":
     main()
