b4252905604f00299ef655ac46bb89cd30a791b7
raunak-sapariya <raunaksapariya@gmail.com>
2024-04-07 06:26:02 +0530

    resss

--- app/main.py
+++ app/main.py
@@ -1,40 +1,38 @@
 import socket
 
-
-def Request(data):
-    data_str = data.decode()
-    split_lines = data_str.split("\r\n")
-    method, path, version = split_lines[0].split()
-    return method, path, version
-
+class Request(object):
+    def __init__(self, data) -> None:
+        data_str = data.decode()
+        lines = data_str.split("\r\n")
+        method, path, version = lines[0].split()
+        self.method = method
+        self.path = path
+        self.version = version
 
 def main():
+    
     server_socket = socket.create_server(("0.0.0.0", 4221))
     while True:
-        client_con, add = server_socket.accept()
-
-        with client_con:
-            print("Client_Connection_Detail  -->", client_con)
-            print("Client_Address_Detail -->", add)
-            print(f'Connected by {add}')
-
-            data = client_con.recv(1024)
-            print("Data -->", data)
-
-
-            response = b"HTTP/1.1 200 OK\r\n\r\n"
-            client_con.send(response)
-
-            method, path, version = Request(data)
-            if path == "/":
-                response1 = b"HTTP/1.1 200 OK\r\n\r\n"
-                
-            else:
-                response1 = b"HTTP/1.1 404 Not Found\r\n\r\n"
-            client_con.send(response1)
-
-    server_socket.close()
-
-
+        conn, addr = server_socket.accept()  
+        with conn:
+            print("Connected by", addr)
+            data = conn.recv(1024)
+            print(data)
+            resp = "HTTP/1.1 200 OK\r\n\r\n".encode()
+            conn.send(resp)
+        try:
+            conn, addr = server_socket.accept()  
+            with conn:
+                print("Connected by", addr)
+                data = conn.recv(1024)
+                print("data: %s" % data)
+                req = Request(data)
+                if req.path == "/":
+                    resp = "HTTP/1.1 200 OK\r\n\r\n"
+                else:
+                    resp = "HTTP/1.1 404 Not Found\r\n\r\n"
+                conn.send(resp.encode())
+        except Exception as e:
+            print(e)
 if __name__ == "__main__":
     main()
