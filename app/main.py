88fded500cad83d20a747f0cab47cd2b7cd1cd83
raunak-sapariya <raunaksapariya@gmail.com>
2024-04-11 12:43:57 +0530

    finalgggxxsdlnsdlvnsddfdfddfdssvcxcxc

--- app/main.py
+++ app/main.py
@@ -118,8 +118,8 @@ def handle_conn(client_conn,addr,directory):
 
             elif req[0]=="POST" and req[1].startswith("/files/"):
                 file_path=os.path.join(directory,req[1][7:])
-
-                file_content=req[4][7]
+                print(file_path)
+                file_content=req[4][7:]
                 print(file_content)
 
                 if os.path.exists(file_path):
