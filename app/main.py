ef5c11bfdf143711abad1ee0b58d63c2a8185237
raunak-sapariya <raunaksapariya@gmail.com>
2024-04-11 13:37:46 +0530

    finalgggxxsdlnsdlvnsddfdfddfdssvcxcxc

--- app/main.py
+++ app/main.py
@@ -117,7 +117,7 @@ def handle_conn(client_conn,addr,directory):
 
             elif req[0] == "POST" and req[1].startswith("/files/"):
                 print("vdvdvdvdv")
-                file_path=os.path.join(os.getcwd(),req[1][7:])
+                file_path=os.path.join(directory,req[1][7:])
                 print(file_path)
                 file_content=req[-1][-1]
                 print(file_content)
