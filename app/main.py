5ec2d518f8437e0b39bcd76d5ac94a94341e41ca
raunak-sapariya <raunaksapariya@gmail.com>
2024-07-15 09:05:46 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -56,7 +56,6 @@ def handle_conn(client_conn,addr,directory):
                             f'User-Agent: {user_agent}',
                             f"Accept-Encoding: {accept_encoding}",
                             f"Content-Encoding: gzip",
-                            "",
                             gzip_content,
                     ]).encode() 
                 else :
